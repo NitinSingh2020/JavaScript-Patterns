61dbbb229424df15fdebb3751ab3cb03928352ca
Nitin Singh <nitin.singh2020@gmail.com>
2015-04-05 21:52:48 -0500

    Added solution to problem 7

---
+++ learnyounode/exercise_7.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+http.get(process.argv[2], function(response) {
+	//console.log("Got response: " + response.statusCode);
+	response.on("data", function (data) { console.log(data.toString()); })
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
+/*
+var http = require('http')
+
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8')
+  response.on('data', console.log)
+  response.on('error', console.error)
+})
+*/
